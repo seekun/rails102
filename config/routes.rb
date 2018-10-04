d67407f2d6f866f5f2329d86094b7cfca66490bb
seekun <tmsshikun@gmail.com>
2018-10-04 10:07:16 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,14 @@ Rails.application.routes.draw do
 
     resources :posts
   end
+
+  # namespace :account do
+  #   resource :groups
+  # end
+  # 把resources写成了resource, 所以报错找不到show action
+  namespace :account do
+    resources :groups
+  end
+
+
 end
